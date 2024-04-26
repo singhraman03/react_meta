902bb1d3a8890f8addf407d46a34cded3609ed23
Raman <rs359388@gmail.com>
2024-04-26 14:29:23 +0530

    img import

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import Intro3 from "./Components/Intro3";
 import Footer from "./Components/Footer";
 import "./App.css";
 import "./index.js";
-import image from "./image.png";
+// import image from "./image.png";
 import Card from "./Components/Card";
 import Btn from "./Components/Btn";
 import ModeToggler from "./Components/ModeToggler";
@@ -32,19 +32,21 @@ import FruitsCounter from './Components/FruitsCounter';
 import Contact from './Components/Contact';
 import Homepage from './Components/Homepage';
 import AboutMe  from './Components/AboutMe';
-import {routes, route, Link} from 'react-router-dom';
+// import {routes, route, Link} from 'react-router-dom';
+import Displaypic from './Components/Displaypic';
 
 
 
-function Logo(props){
-  const myLogo = <img src= {image}/>;
-  return myLogo;
-}
+
+// function Logo(props){
+//   const myLogo = <img src= {image}/>;
+//   return myLogo;
+// }
 
 
 function App(){
   return (
-    <div className="App">
+    <>
       <nav className='Nav-items'>
         <link to="/" className="Nav-items">Homepage</link>
         <link to="/About" className='Nav-items'>About Me</link>
@@ -55,21 +57,23 @@ function App(){
         <route path="/AboutMe" element={<AboutMe/>}></route>
         <route path="/Contact" element={<Contact/>}></route>
       </routes>
-      <Logo/>
+      
       <h1> Task: Add three card elements </h1>
       <Card h2="First card's h2" h3="First card's h3"/>
       <Card h2="second card's h2" h3="second card's h3"/>
       <Card h2="third card's h3" h3="third card's h3"/>
 
-      <> 
+      <Displaypic/>
+
+       
       <Btn className='button'/>
-      </>
+      
       <ModeToggler/>
       <Button/>
       <InputComponent/>
       <RegisterForm/>
       <ApiCalls/>
-      <h1>Where should the state go?</h1>\
+      <h1>Where should the state go?</h1>
       <Fruits Fruits= {Fruits}/>
       <FruitsCounter Fruits= {Fruits}/>
 
@@ -80,7 +84,7 @@ function App(){
       <Intro2 type="business"/>
       <Intro3 type="like"/>
       <Footer name="shopping cart" color="blue"/>
-    </div>
+    </>
     
   )
 };
---
+++ src/Components/Displaypic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import image1 from './Assests/images';
+
+const Displaypic = () => {
+  return (
+    <div>Displaypic
+      <img src={require('./Assests/images/image1.avif')} alt='Nature'/>
+    </div>
+  )
+}
+
+export default Displaypic
